90d61467c4c303efb3b9db839562fa5fd68535f5
NOOTHI AKSHITH <125175973+noothiakshith@users.noreply.github.com>
2024-05-12 12:57:28 +0000

    feat: Update accounts API to filter by user ID
    This commit modifies the accounts API endpoint in the `accounts.ts` file to include a filter by user ID. It adds the `eq` function from the `drizzle-orm` library to the import statements and updates the query to include a `where` clause that filters the accounts by the authenticated user's ID.
    
    Recent user commits:
    - good and working
    - chore: Update API response message
    - chore: Update npm dependencies and add TypeScript compiler
    - feat: Add database migration and generation scripts
    
    Recent repository commits:
    - chore: Update npm dependencies
    - 3rd check
    - 2nd check
    - first
    - Init App

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -2,6 +2,7 @@ import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { error } from "console";
+import { eq } from "drizzle-orm";
 import {Hono} from "hono";
 import {HTTPException} from  "hono/http-exception";
 const app = new Hono()
@@ -17,7 +18,8 @@ const app = new Hono()
         id:accounts.id,
         name:accounts.name
     })
-    .from(accounts);
+    .from(accounts)
+    .where(eq(accounts.userId,auth.userId));
     return c.json({data})
 })
 
