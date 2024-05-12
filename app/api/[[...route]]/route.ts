68ea08bf3276620384b19d55997c917d05579cb6
NOOTHI AKSHITH <125175973+noothiakshith@users.noreply.github.com>
2024-05-12 12:01:47 +0000

    good and working

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,14 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { z } from "zod";
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import accounts from "./accounts";
+export const runtime = "edge";
 
-export const runtime = 'edge';
+const app = new Hono().basePath("/api");
 
-const app = new Hono().basePath('/api')
+const routes = app.route("/accounts", accounts);
+// Exporting route handlers
+export const GET = handle(app);
+export const POST = handle(app);
 
-app.get('/hello', (c) => {
-  return c.json({
-    message: 'Nik enduku ra jaffa',
-  })
-})
-
-export const GET = handle(app)
-export const POST = handle(app)
+export type AppType = typeof routes;
